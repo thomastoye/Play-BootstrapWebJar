319c32cb1551b1944e4249728df6750e37b38721
Thomas Toye <toye.thomas@gmail.com>
2014-04-28 20:00:01 +0200

    Added simple form example in the index view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,12 +3,17 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import models.Order;
+import play.data.Form;
+
 import views.html.*;
 
 public class Application extends Controller {
 
+    public static final Form<Order> orderForm = Form.form(Order.class);
+
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Your new application is ready.", orderForm));
     }
 
 }
